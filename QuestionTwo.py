2780a4362a5c78a584989df7c88576068122c6c0
yadavpanda2266 <yadavpanda2266@gmail.com>
2025-07-27 07:55:46 +0530

    Add files via upload

---
+++ QuestionTwo.py
@@ -0,0 +1,17 @@
+'''
+num = int(input('Enter your number : '))
+
+if (num % 2 == 1):
+    print(num,'is  odd number')
+
+else:
+    print(num,'is an even number')
+'''
+
+#sum of integers from 1 to 50 using for loop
+
+total = 0
+for i in range(1,51):
+    total += i
+print('The sum of integers of From 1 to 50 : ', total)
+
